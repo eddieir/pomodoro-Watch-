090ae7ae55af203b9876741ea4b188c8be774dc7
peyman iravani <peyman.iravani@gmail.com>
2026-08-20 09:17:27 +0200

    Refresh PWA cache for rainforest audio

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tomato-watch-daisy-v2';
+const CACHE_NAME = 'tomato-watch-daisy-v3';
 const ASSETS = ['/', '/index.html', '/styles.css', '/app.js', '/manifest.webmanifest', '/icon.svg'];
 
 self.addEventListener('install', event => {
